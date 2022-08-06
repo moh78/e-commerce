3db3d89317224e9203c93de0724c37c6aa08e1d0
khelif mohammed <medkheli@gmail.com>
2022-08-06 12:30:16 +0200

    asset secure

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,16 +5,16 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Home</title>	
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('assets/images/favicon.ico')}}">
+    <link rel="shortcut icon" type="image/x-icon" href="{{secure_asset('assets/images/favicon.ico')}}">
 	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,400italic,700,700italic,900,900italic&subset=latin,latin-ext" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open%20Sans:300,400,400italic,600,600italic,700,700italic&subset=latin,latin-ext" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/animate.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/font-awesome.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/bootstrap.min.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/owl.carousel.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/animate.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/font-awesome.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/bootstrap.min.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/owl.carousel.min.css') }}">
 	{{-- <link rel="stylesheet" type="text/css" href="{{ asset('assets/css/chosen.min.css') }}"> --}}
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/style.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('assets/css/color-01.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/style.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('assets/css/color-01.css') }}">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.css" integrity="sha512-cOGz9gyEibwgs1MVDCcfmQv6mPyUkfvrV9TsRbTuOA12SQnLzBROihf6/jK57u0YxzlxosBFunSt4V75K6azMw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.6.0/nouislider.min.css" integrity="sha512-qveKnGrvOChbSzAdtSs8p69eoLegyh+1hwOMbmpCViIwj7rn4oJjdmMvWOuyQlTOZgTlZA0N2PXA7iA8/2TUYA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     @livewireStyles
@@ -226,16 +226,16 @@
 	@livewire('footer-component')
 
 	@livewireScripts
-	<script src="{{ asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
-	<script src="{{ asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
-	<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.flexslider.js') }}"></script>
-	<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/jquery-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
+	<script src="{{ secure_asset('assets/js/jquery-ui-1.12.4.minb8ff.js?ver=1.12.4') }}"></script>
+	<script src="{{ secure_asset('assets/js/bootstrap.min.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/jquery.flexslider.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/owl.carousel.min.js') }}"></script>
 	{{-- <script src="{{ asset('assets/js/jquery.countdown.min.js') }}"></script> --}}
-	<script src="{{ asset('assets/js/jquery.sticky.js') }}"></script>
-	<script src="{{ asset('assets/js/momentjs.js') }}"></script>
-	<script src="{{ asset('assets/js/jquery.sticky.js') }}"></script>
-	<script src="{{ asset('assets/js/functions.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/jquery.sticky.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/momentjs.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/jquery.sticky.js') }}"></script>
+	<script src="{{ secure_asset('assets/js/functions.js') }}"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/noUiSlider/15.6.0/nouislider.min.js" integrity="sha512-1mDhG//LAjM3pLXCJyaA+4c+h5qmMoTc7IuJyuNNPaakrWT9rVTxICK4tIizf7YwJsXgDC2JP74PGCc7qxLAHw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> 
 </body>
 </html>
